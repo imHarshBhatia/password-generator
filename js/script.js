6c49634d5928e697201236d07933893e7935337e
Harsh Bhatia <harshbhatia2007@gmail.com>
2021-02-10 03:30:04 +0530

    copy password bug fix and code refactoring

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+var env = 'prod';
 const numbers = '0123456789';
 const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -41,8 +42,8 @@ function generatePassowrd() {
   let passwordAndChars = createPwdAndChars();
   let password, passwordChars;
   [password, passwordChars] = [passwordAndChars.password, passwordAndChars.passwordChars];
-  console.log(`Password is: ${password}`);
-  console.log(`password chars are ${passwordChars}`);
+  log(`Password is: ${password}`);
+  log(`password chars are ${passwordChars}`);
   if (!passwordChars) {
     window.alert('A single type of character needs to be selected for password to be genrated');
     return;
@@ -51,25 +52,25 @@ function generatePassowrd() {
   const generatePasswordLength = lengthSlider.value - password.length;
   const noRepetitiveChars = lengthSlider.value < passwordChars.length;
   for (let i = 0; i < generatePasswordLength; i++) {
-    console.log(`Iteration: ${++count}`);
-    console.log(`Inside for loop, password is: ${password}`);
+    log(`Iteration: ${++count}`);
+    log(`Inside for loop, password is: ${password}`);
     const generatedPasswordChar = passwordChars[Math.ceil(Math.random() * passwordCharsLength)];
-    console.log(`Generated password char is: ${generatedPasswordChar}`);
+    log(`Generated password char is: ${generatedPasswordChar}`);
     if (noRepetitiveChars) {
       if (password.includes(generatedPasswordChar)) {
-        console.log('Repetitive chars found');
+        log('Repetitive chars found');
         i--;
         continue;
       }
     }
-    console.log(`sliced char: ${password.slice(-1)}`);
+    log(`sliced char: ${password.slice(-1)}`);
     if (password.slice(-1) === generatedPasswordChar) {
       i--;
-      console.log('Not executing as two chars are same');
+      log('Not executing as two chars are same');
       continue;
     }
     password += generatedPasswordChar;
-    console.log(`Password after iteration is: ${password}`);
+    log(`Password after iteration is: ${password}`);
   }
   passwordTextBox.value = password;
   disablePwdCopy();
@@ -103,11 +104,13 @@ function createPwdAndChars() {
 }
 
 function copyPasswordToClipBoard() {
-  console.log('Called copy');
+  log('Called copy');
+  passwordTextBox.removeAttribute('disabled');
   passwordTextBox.select();
   passwordTextBox.setSelectionRange(0, 99999);
   document.execCommand('copy');
   document.getSelection().removeAllRanges();
+  passwordTextBox.setAttribute('disabled', true);
   M.toast({html: 'Password copied to clipboard'});
 }
 
@@ -116,4 +119,10 @@ function copyPasswordToClipBoard() {
 function rotateIcon(angle) {
   rotationDegrees += angle;
   $('#generate-password i').css('transform', 'rotate(' + rotationDegrees + 'deg)');
+}
+
+function log(comment) {
+  if (env === 'dev') {
+    console.log(comment);
+  }
 }
